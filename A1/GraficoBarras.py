a46e0e62c6af3d3c3e5c183fd8177f0bf1366680
Gustavo Mata <gustavo.mata@clouddog.com.br>
2020-01-19 18:16:41 -0300

    Comparando Grafico de barras

--- A1/GraficoBarras.py
+++ A1/GraficoBarras.py
@@ -1,12 +1,18 @@
 import matplotlib.pyplot as plt
 
-x = [1, 2, 3, 4, 5]
-y = [2, 3, 7, 1, 10]
+x1 = [1, 2, 3, 4, 5]
+y1 = [2, 3, 7, 1, 10]
+
+x2 = [2, 4, 6, 8, 10]
+y2 = [5, 1, 3, 7, 4]
 
 #Titulo / Legendas
-plt.title("Meu primeiro gráfico com Python!")
+plt.title("Comparaçao de gráficos de barras")
 plt.xlabel("Eixo X")
 plt.ylabel("Eixo Y")
 
-plt.bar(x,y)
+plt.bar(x1, y1, label = "Grupo 1")
+plt.bar(x2, y2, label = "Grupo 2")
+plt.legend()
+
 plt.show()
